2938e06d3688a4190ae9a10bd3d0f4f1ad687750
RalphBilay <1224139@student.roc-nijmegen.nl>
2026-09-16 11:07:10 +0200

    initial comit

---
+++ src/basisProgameren/mastermind.java
@@ -0,0 +1,127 @@
+package basisProgameren;
+
+public class mastermind {
+
+	public static void main(String[] args) {
+		// string speler1 = code kraker
+		string speller2 = code maker
+
+				string blouwePion = blouw
+				string groenePion =  groen
+				string gelePion = geel
+				string rodePion = rood
+
+				string zwartePin = zwart
+				string witePin = wit
+				string geenPin = niks
+
+				string verborgenRijVak1 = 
+				string verborgenRijVak2 = 
+				string verborgenRijVak3 = 
+				string verborgenRijVak4 = 
+
+				string rij1Vak1 = 
+				string rij1Vak2 = 
+				string rij1Vak3 = 
+				string rij1Vak4 = 
+
+				string rij2Vak1 =
+				string rij2Vak2 =
+				string rij2Vak3 =
+				string rij2Vak4 =
+
+				string rij3Vak1 =""
+				string rij3Vak2 = ""
+				string rij3Vak3 =""
+				string rij3Vak4 =""
+
+				string rij4Vak1 =""
+				string rij4Vak2 =""
+				string rij4Vak3 =""
+				string rij4Vak4 =""
+
+				string rij5Vak1 =""
+				string rij5Vak2 =""
+				string rij5Vak3 =""
+				string rij5Vak4 =""
+
+				string rij6Vak1 =""
+				string rij6Vak2 =""
+				string rij6Vak3 =""
+				string rij6Vak4 =""
+
+				string rij7Vak1 =""
+				string rij7Vak2 = ""
+				string rij7Vak3 =""
+				string rij7Vak4 =""
+
+				string rij8Vak1 =""
+				string rij8Vak2 =""
+				string rij8Vak3 =""
+				string rij8Vak4 =""
+
+				string rij9Vak1 =""
+				string rij9Vak2 =""
+				string rij9Vak3 =""
+				string rij9Vak4 =""
+
+				string rij10Vak1 =""
+				string rij10Vak2 =""
+				string rij10Vak3 =""
+				string rij10Vak4 =""
+
+				string controlerRij1Vak1 =""
+				string controlerRij1Vak2 =""
+				string controlerRij1Vak3 =""
+				string controlerRij1Vak4 ="";
+
+				string controlerRij2Vak1 ="";
+				string controlerRij2Vak2 ="";
+				string controlerRij2Vak3 ="";
+				string controlerRij2Vak4 ="";
+
+				string controlerRij3Vak1 ="";
+				string controlerRij3Vak2 ="";
+				string controlerRij3Vak3 ="";
+				string controlerRij3Vak4 ="";
+
+				string controlerRij4Vak1 ="";
+				string controlerRij4Vak2 ="";
+				string controlerRij4Vak3 ="";
+				string controlerRij4Vak4 ="";
+
+				string controlerRij5Vak1 ="";
+				string controlerRij5Vak2 ="";
+				string controlerRij5Vak3 ="";
+				string controlerRij5Vak4 ="";
+
+				string controlerRij6Vak1 ="";
+				string controlerRij6Vak2 ="";
+				string controlerRij6Vak3 ="";
+				string controlerRij6Vak4 ="";
+
+				string controlerRij7Vak1 ="";
+				string controlerRij7Vak2 ="";
+				string controlerRij7Vak3 ="";
+				string controlerRij7Vak4 ="";
+
+				string controlerRij8Vak1 ="";
+				string controlerRij8Vak2 ="";
+				string controlerRij8Vak3 ="";
+				string controlerRij8Vak4 ="";
+
+				string controlerRij9Vak1 ="";
+				string controlerRij9Vak2 ="";
+				string controlerRij9Vak3 ="";
+				string controlerRij9Vak4 ="";
+
+				string controlerRij10Vak1 ="";
+				string controlerRij10Vak2 ="";
+				string controlerRij10Vak3 ="";
+				string controlerRij10Vak4 ="";
+
+		
+
+	}
+
+}
