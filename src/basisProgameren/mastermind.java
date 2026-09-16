9ec02661eeb814be2f6d4cc1cfdc37edba929784
RalphBilay <1224139@student.roc-nijmegen.nl>
2026-09-16 11:17:38 +0200

    fixed code

--- src/basisProgameren/mastermind.java
+++ src/basisProgameren/mastermind.java
@@ -1,124 +1,126 @@
 package basisProgameren;
 
+import java.swing.Spring;
+
 public class mastermind {
 
 	public static void main(String[] args) {
-		// string speler1 = code kraker
-		string speller2 = code maker
+		String speler1 = "code kraker";
+		String speller2 = "code maker";
 
-				string blouwePion = blouw
-				string groenePion =  groen
-				string gelePion = geel
-				string rodePion = rood
+				String blouwePion = "blouw";
+				String groenePion =  "groen";
+				String gelePion = "geel";
+				String rodePion = "rood";
 
-				string zwartePin = zwart
-				string witePin = wit
-				string geenPin = niks
+				String zwartePin = "zwart";
+				String witePin = "wit";
+				String geenPin = "niks";
 
-				string verborgenRijVak1 = 
-				string verborgenRijVak2 = 
-				string verborgenRijVak3 = 
-				string verborgenRijVak4 = 
+				String verborgenRijVak1 = "";
+				String verborgenRijVak2 = "";
+				String verborgenRijVak3 = "";
+				String verborgenRijVak4 = "";
 
-				string rij1Vak1 = 
-				string rij1Vak2 = 
-				string rij1Vak3 = 
-				string rij1Vak4 = 
+				String rij1Vak1 = "";
+				String rij1Vak2 = "";
+				String rij1Vak3 = "";
+				String rij1Vak4 = "";
+				
+				String rij2Vak1 ="";
+				String rij2Vak2 ="";
+				String rij2Vak3 ="";
+				String rij2Vak4 ="";
 
-				string rij2Vak1 =
-				string rij2Vak2 =
-				string rij2Vak3 =
-				string rij2Vak4 =
+				String rij3Vak1 ="";
+				String rij3Vak2 = "";
+				String rij3Vak3 ="";
+				String rij3Vak4 ="";
+				
+				String rij4Vak1 ="";
+				String rij4Vak2 ="";
+				String rij4Vak3 ="";
+				String rij4Vak4 ="";
+				
+				String rij5Vak1 ="";
+				String rij5Vak2 ="";
+				String rij5Vak3 ="";
+				String rij5Vak4 ="";
 
-				string rij3Vak1 =""
-				string rij3Vak2 = ""
-				string rij3Vak3 =""
-				string rij3Vak4 =""
+				String rij6Vak1 ="";
+				String rij6Vak2 ="";
+				String rij6Vak3 ="";
+				String rij6Vak4 ="";
 
-				string rij4Vak1 =""
-				string rij4Vak2 =""
-				string rij4Vak3 =""
-				string rij4Vak4 =""
+				String rij7Vak1 ="";
+				String rij7Vak2 = "";
+				String rij7Vak3 ="";
+				String rij7Vak4 ="";
 
-				string rij5Vak1 =""
-				string rij5Vak2 =""
-				string rij5Vak3 =""
-				string rij5Vak4 =""
+				String rij8Vak1 ="";
+				String rij8Vak2 ="";
+				String rij8Vak3 ="";
+				String rij8Vak4 ="";
 
-				string rij6Vak1 =""
-				string rij6Vak2 =""
-				string rij6Vak3 =""
-				string rij6Vak4 =""
+				String rij9Vak1 ="";
+				String rij9Vak2 ="";
+				String rij9Vak3 ="";
+				String rij9Vak4 ="";
 
-				string rij7Vak1 =""
-				string rij7Vak2 = ""
-				string rij7Vak3 =""
-				string rij7Vak4 =""
+				String rij10Vak1 ="";
+				String rij10Vak2 ="";
+				String rij10Vak3 ="";
+				String rij10Vak4 ="";
 
-				string rij8Vak1 =""
-				string rij8Vak2 =""
-				string rij8Vak3 =""
-				string rij8Vak4 =""
+				String controlerRij1Vak1 ="";
+				String controlerRij1Vak2 ="";
+				String controlerRij1Vak3 ="";
+				String controlerRij1Vak4 ="";
 
-				string rij9Vak1 =""
-				string rij9Vak2 =""
-				string rij9Vak3 =""
-				string rij9Vak4 =""
+				String controlerRij2Vak1 ="";
+				String controlerRij2Vak2 ="";
+				String controlerRij2Vak3 ="";
+				String controlerRij2Vak4 ="";
 
-				string rij10Vak1 =""
-				string rij10Vak2 =""
-				string rij10Vak3 =""
-				string rij10Vak4 =""
+				String controlerRij3Vak1 ="";
+				String controlerRij3Vak2 ="";
+				String controlerRij3Vak3 ="";
+				String controlerRij3Vak4 ="";
+				
+				String controlerRij4Vak1 ="";
+				String controlerRij4Vak2 ="";
+				String controlerRij4Vak3 ="";
+				String controlerRij4Vak4 ="";
+				
+				String controlerRij5Vak1 ="";
+				String controlerRij5Vak2 ="";
+				String controlerRij5Vak3 ="";
+				String controlerRij5Vak4 ="";
 
-				string controlerRij1Vak1 =""
-				string controlerRij1Vak2 =""
-				string controlerRij1Vak3 =""
-				string controlerRij1Vak4 ="";
+				String controlerRij6Vak1 ="";
+				String controlerRij6Vak2 ="";
+				String controlerRij6Vak3 ="";
+				String controlerRij6Vak4 ="";
 
-				string controlerRij2Vak1 ="";
-				string controlerRij2Vak2 ="";
-				string controlerRij2Vak3 ="";
-				string controlerRij2Vak4 ="";
+				String controlerRij7Vak1 ="";
+				String controlerRij7Vak2 ="";
+				String controlerRij7Vak3 ="";
+				String controlerRij7Vak4 ="";
+				
+				String controlerRij8Vak1 ="";
+				String controlerRij8Vak2 ="";
+				String controlerRij8Vak3 ="";
+				String controlerRij8Vak4 ="";
+				
+				String controlerRij9Vak1 ="";
+				String controlerRij9Vak2 ="";
+				String controlerRij9Vak3 ="";
+				String controlerRij9Vak4 ="";
 
-				string controlerRij3Vak1 ="";
-				string controlerRij3Vak2 ="";
-				string controlerRij3Vak3 ="";
-				string controlerRij3Vak4 ="";
-
-				string controlerRij4Vak1 ="";
-				string controlerRij4Vak2 ="";
-				string controlerRij4Vak3 ="";
-				string controlerRij4Vak4 ="";
-
-				string controlerRij5Vak1 ="";
-				string controlerRij5Vak2 ="";
-				string controlerRij5Vak3 ="";
-				string controlerRij5Vak4 ="";
-
-				string controlerRij6Vak1 ="";
-				string controlerRij6Vak2 ="";
-				string controlerRij6Vak3 ="";
-				string controlerRij6Vak4 ="";
-
-				string controlerRij7Vak1 ="";
-				string controlerRij7Vak2 ="";
-				string controlerRij7Vak3 ="";
-				string controlerRij7Vak4 ="";
-
-				string controlerRij8Vak1 ="";
-				string controlerRij8Vak2 ="";
-				string controlerRij8Vak3 ="";
-				string controlerRij8Vak4 ="";
-
-				string controlerRij9Vak1 ="";
-				string controlerRij9Vak2 ="";
-				string controlerRij9Vak3 ="";
-				string controlerRij9Vak4 ="";
-
-				string controlerRij10Vak1 ="";
-				string controlerRij10Vak2 ="";
-				string controlerRij10Vak3 ="";
-				string controlerRij10Vak4 ="";
+				String controlerRij10Vak1 ="";
+				String controlerRij10Vak2 ="";
+				String controlerRij10Vak3 ="";
+				String controlerRij10Vak4 ="";
 
 		
 
